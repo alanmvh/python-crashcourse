20c9fca9e01a240818b79d01aa59701326b58328
Alan Villarreal <alanmvh@gmail.com>
2022-07-07 00:02:56 -0500

    Added more class attributes

--- Classes/electric_car.py
+++ Classes/electric_car.py
@@ -1,10 +1,25 @@
 import car
 
 class ElectricCar(car.Car):
-    """Represents aspects of a car specific to electric vehicles"""
+    """
+    Initialize attributes of the parent class.
+    Then initialize attributes specific to an electric car
+    """
+
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 70
     
+    def describe_battery(self):
+        """
+        Print a statement describing the battery size
+        """
+        print("This car has a " + str(self.battery_size) + "-kWh battery")
+    
+    def fill_gast_tank():
+        """
+        Electric cars dont have gas tanks
+        """
 my_tesla = ElectricCar('tesla', 'model s', '2016')
 print(my_tesla.get_descriptive_name())
