2b5c91f0525445937db4ee5c79994b94f678350f
Alan Villarreal <alanmvh@gmail.com>
2022-07-07 00:07:30 -0500

    Battery class created to use as car attribute

---
+++ Classes/battery.py
@@ -0,0 +1,12 @@
+class Battery():
+    """A simple attempt to model a battery for an electric car"""
+
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print("This car has a" + str(self.battery_size) + "-kWh battery")
+
+        
--- Classes/electric_car.py
+++ Classes/electric_car.py
@@ -1,3 +1,4 @@
+from battery import Battery
 import car
 
 class ElectricCar(car.Car):
@@ -9,7 +10,7 @@ class ElectricCar(car.Car):
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 70
+        self.battery = Battery()
     
     def describe_battery(self):
         """
@@ -22,4 +23,5 @@ class ElectricCar(car.Car):
         Electric cars dont have gas tanks
         """
 my_tesla = ElectricCar('tesla', 'model s', '2016')
+my_tesla.battery.describe_battery()
 print(my_tesla.get_descriptive_name())
