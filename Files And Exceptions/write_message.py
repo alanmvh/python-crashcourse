8725080cf53c074e31ee722d780e685e81920316
Alan Villarreal <alanmvh@gmail.com>
2022-07-13 23:27:51 -0500

    Writing to an empty file

---
+++ Files And Exceptions/write_message.py
@@ -0,0 +1,5 @@
+filename = "C:\\Users\\Nydhoggr\\Desktop\\Programming\\Python\\Files And Exceptions\\programming.txt"
+
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming"*2)
