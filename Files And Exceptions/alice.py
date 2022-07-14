a482051721192228dc1ae40579e49021a8a02d02
Alan Villarreal <alanmvh@gmail.com>
2022-07-14 00:10:49 -0500

    FileNotFound exception sample

---
+++ Files And Exceptions/alice.py
@@ -0,0 +1,10 @@
+from fileinput import filename
+
+
+file_name = "Files And Exceptions/alice.txt"
+try:
+    with open(file_name) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "Sorry, the file " + file_name + " does not exist"
+    print(msg)
