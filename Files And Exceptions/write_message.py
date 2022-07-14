07c9810b58f8221514af4655c3de5344b4fa76bf
Alan Villarreal <alanmvh@gmail.com>
2022-07-13 23:42:28 -0500

    Modify write to append mode

--- Files And Exceptions/write_message.py
+++ Files And Exceptions/write_message.py
@@ -1,5 +1,6 @@
 filename = "Files And Exceptions/programming.txt"
 
 
-with open(filename, 'w') as file_object:
+with open(filename, 'a') as file_object:
     file_object.write("I love programming"*2)
+    file_object.write("I love creating apps that can run in a browser.\n")
