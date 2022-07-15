aba22501b8ae3727cac9a977d87d4aec3ba6933e
Alan Villarreal <alanmvh@gmail.com>
2022-07-14 23:40:23 -0500

    Added counter to guest loop

--- Files And Exceptions/guest_book.py
+++ Files And Exceptions/guest_book.py
@@ -1,8 +1,10 @@
 file_path = "Files And Exceptions/guest_book.txt"
 
 with open(file_path, 'a') as file_object:
+    i = 0
     while(True):
+        i += 1
         guest_name = input("Write guest's name: \nIf you want to quit type 'q' and press enter\n")
         if guest_name == 'q':
             break
-        file_object.write("Guest: " + guest_name + "\n")
+        file_object.write("Guest: " + i + guest_name + "\n")
