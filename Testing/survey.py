18cc84dd0021f461886d18dc21dea2ad9f9fbb06
Alan Villarreal <alanmvh@gmail.com>
2022-07-21 00:52:20 -0500

    unittests for survey class

--- Testing/survey.py
+++ Testing/survey.py
@@ -1,21 +1,21 @@
 class AnonymousSurvey():
     """Collect anonymous answers to a survey question"""
 
-    def __init(self, question):
+    def __init__(self, question):
         """Store a question and prepare to store responses"""
-        self.question = question = question
-        self.respones = []
+        self.question = question
+        self.responses = []
 
     def show_question(self):
         """Show the survey question"""
-        print(question)
+        print(self.question)
 
     def store_response(self, new_response):
         """Store a single response to the survey"""
-        self.respones.append(new_response)
+        self.responses.append(new_response)
         
     def show_results(self):
-        """Show al the responses that have been given"""
+        """Show al the responses tht have been given"""
         print("Survey results")
-        for response in responses:
-            print('-' + response)
+        for response in self.responses:
+            print('- ' + response)  
